221a3d034c60da7d19cf09e3c0c11582bcbb54b5
LordNecoReal <lordconhecimento@gmail.com>
2026-03-18 19:31:28 -0300

    Hospital removido

--- database.js
+++ database.js
@@ -31,14 +31,23 @@ const criarBanco = async () => {
   // Insert - C do CRUD - CREATE
   // ============================
 
-  await db.exec(`
+
+
+ const checagem = await db.get(`SELECT COUNT (*) AS total FROM incidentes`);
+  if (checagem.total === 0) {
+    await db.exec(`
     INSERT INTO incidentes(tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro) VALUES
-    ("Iluminação", "Rua das Flores, 123, Bairro das Margaridas", "Poste queimado há dias", "Média", "Ana Clara", "2026-03-16", "10:30"),
-    ("Falta de energia", "Hospital JP2", "Local na escuridão", "Alta", "Antônio Perna Quebrada", "2026-03-16", "22:15"),
+    ("Iluminação", "Rua das Flores, 123, Bairro das Margaridas", "Poste queimado há dias", "Média", "Ana Clara", "16/03/2026", "10:30"),
+    ("Falta de energia", "Hospital JP2", "Local na escuridão", "Alta", "Antonio Perna Quebrada", "16/03/2026", "22:15"),
     ("Vazamento de água", "Rua das Camélias, 52", "Vazamento de água constante próximo ao bueiro.", "Alta", "Julia Martins", "2026-03-16", "10:00"),
-    ("Pavimentação", "Avenida C, Bairro D", "Calçada em mau estado", "Alta", "Maria Oliveira", "2026-03-14", "14:30"),
-    ("Falta de água", "Rua T, 146, Jardim Imbariê", "Moradores sem água", "Alta", "Dona Fofoca", "2026-03-16", "10:00")
-`);
+    ("Pavimentação", "Avenida C, Bairro D","Calçada em mau estado", "Alta", "Maria Oliveira","14/03/2026", "14:30"),
+    ("Falta de água", "Rua T, 146, Jardim Imbarie", "Moradores sem água", "Alta", "Dona Fofoca", "16/03/2026", "10:00")
+    `);
+  } else {
+    console.log(`Banco pronto com ${checagem.total} de incidentes `);
+  }
+
+ 
 
   // ============================
   // Select  - R do CRUD - READ
@@ -47,8 +56,43 @@ const criarBanco = async () => {
   const todosOsIncidentes = await db.all("SELECT * FROM incidentes");
   console.table(todosOsIncidentes);
 
-  // É sempre bom fechar a conexão após o uso
-  await db.close();
+//Exemplo de SELECT especifico
+
+const chamadosAna =await db.all(`SELECT * FROM incidentes WHERE nome_solicitante = "Ana Clara" `) 
+console.table(chamadosAna)
+
+//UPDATE
+
+await db.run(`
+  UPDATE incidentes 
+  SET status_resolucao = "Em Análise"
+  WHERE data_registro = "16/03/2026"
+  
+  `)
+console.log("Todas as reclamações do dia 16/03/2026 tiveram uma atualização" )
+
+//UPDATE
+
+await db.run(`
+  UPDATE incidentes
+  SET status_resolucao = "Resolvido"
+  WHERE tipo_problema = "Falta de energia"
+
+  `)
+  console.log ("Problema no hospital resolvido")
+
+//DELETE
+await db.run(`DELETE FROM incidentes WHERE id = 2`)
+
+console.log("Registro do ID 2 removido");
+
+//Relatório/SELECT Final
+
+console.log("Relatório Atualizado (FINAL)")
+
+const resultadoFinal =await db.all(`SELECT * FROM incidentes`);
+console.table(resultadoFinal)
+
 };
 
 criarBanco();
