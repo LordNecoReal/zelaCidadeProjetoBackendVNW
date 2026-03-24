b433b12b8383bf59df5de5bb993714223786a48a
LordNecoReal <lordconhecimento@gmail.com>
2026-03-24 20:43:43 -0300

    Att

--- server.js
+++ server.js
@@ -1,41 +1,74 @@
 //Importações
 
-const express = require('express') //Framework que cria o servidor e as rotas
-const { criarBanco } = require('./database') //A chave que vai abrir a conexão com o banco de dados
-    
+const express = require("express"); //Framework que cria o servidor e as rotas
+const { criarBanco } = require("./database"); //A chave que vai abrir a conexão com o banco de dados
+
 const app = express(); //Inicialização: Ligando o motor do servidor
 
-app.use(express.json()) //Tradutor: Configura o express para entender dados enviados no formato JSON 
+app.use(express.json()); //Tradutor: Configura o Express para entender dados enviados no formato JSON
 
-//Criando a Rota Principal //Rota Raiz
-app.get('/', (req,res) => {
-//res.send: Envia uma resposta simples (Texto, html, json)
-    res.send(`
-        <body
-<h1> ZelaCidade</h1>
-<h2> Gestão de problemas urbanos </h2>
-<p> Endpoint que leva os incidentes cadastrados: /incidentes </p>      
+//Criando a Rota Principal /Rota Raiz
 
-</body>
+app.get("/", (req, res) => {
+  //res.send: Envia uma resposta simples (texto, html, json)
+  res.send(`
+        <body>
+    <h1> ZelaCidade </h1>
+    <h2> Gestão de Problemas Urbanos </h2>
+    <p> Endpoint que leva aos incidentes cadastrados: /incidentes </p> 
+        </body>
         
         `);
 });
 
 //Porta do servidor
 
-const PORT  = 3000;
+const PORT = 3000;
 
+//Ligando o Servidor
 app.listen(PORT, () => {
-    console.log(` Servidor rodando em http://localhost:${PORT} `)
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
-//Rota de listagem - Para buscar todos os problemas registrados
 
-app.get ("/incidentes", async (req,res) => {
-const db = await criarBanco() //Chamamos a função do outro arquivo. O await é o "aguarde", pois o banco precisa de um tempo para abrir 
+//Rota de Listagem - Para buscar todos os problemas registrados
 
-const listaIncidentes = await db.all('SELECT * FROM incidentes')
+app.get("/incidentes", async (req, res) => {
+
+const db = await criarBanco() // Chamamos a função do outro arquivo. O await é o "aguarde", pois o banco precisa de tempo para abrir. 
+
+const listaIncidentes = await db.all(`SELECT * FROM incidentes`)
 
 res.json(listaIncidentes) //Entrega esses dados para o cliente em formato JSON
 
-})
+} ) 
+
+
+//Rota Específica 
+app.get("/incidentes/:id", async (req, res) => {
+
+const { id } = req.params
+
+const db = await criarBanco()
+
+const incidenteEspecifico = await db.all(`SELECT * FROM incidentes WHERE id = ?`, [id])
+// ? é um espaço reservado que será preenchido pelo valor da variável [id]
+// ? SQL Injection é usado para segurança 
+
+res.json(incidenteEspecifico)
+} )
+
+
+//Rota POST - Novos Registros  /Endpoints 
+
+app.post("/incidentes", async (req,res) => {
+
+const {tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro} = req.body
+
+const db = await criarBanco()
+
+await db.run(`INSERT INTO incidentes(tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro) VALUES (?, ?, ?, ?, ?, ?, ?)`, [tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro])
+
+//Envia uma resposta de confirmação para o cliente que fez a requisição
+res.send(`Incidente novo registrado: ${tipo_problema} registrado na data ${data_registro} por ${nome_solicitante} `)
+} )
